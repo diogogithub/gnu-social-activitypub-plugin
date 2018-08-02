1b1631b5308956fa3fd591962c418845b9933629
Diogo Cordeiro <diogo@fc.up.pt>
2018-08-02 02:16:45 +0100

    Redundant command removed

--- actions/inbox/Create.php
+++ actions/inbox/Create.php
@@ -54,7 +54,7 @@ if (isset($res->longitude)) {
 
 try {
     Activitypub_notice::create_notice(
-        ActivityPub_explorer::get_profile_from_url($data->actor),
+        $actor_profile,
         $res->id,
         $res->url,
         $res->content,
